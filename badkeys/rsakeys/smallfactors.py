a14d6a41ecd81cf3032a3bfd9fbbfd2cd1c73c8c
Hanno Böck <hanno@gentoo.org>
2022-01-14 17:42:25 +0100

    Mark smallprimes as private symbol and calculate in temp var first

--- badkeys/rsakeys/smallfactors.py
+++ badkeys/rsakeys/smallfactors.py
@@ -2,22 +2,23 @@ import gmpy2
 
 MAX_PRIME = 65537
 
-smallprimes = None
+_smallprimes = None
 
 
 def smallfactors(n, e=0):
-    global smallprimes
+    global _smallprimes
 
     # Generate product of all primes <= MAX_PRIME.
     # We calculate this once per program run, we could precalculate the
     # constant, but it's fast enough to calculate on the fly.
-    if smallprimes is None:
-        smallprimes = 2
+    if _smallprimes is None:
+        sp = 2
         for i in range(3, MAX_PRIME + 1, 2):
             if gmpy2.is_prime(i):
-                smallprimes *= i
+                sp *= i
+        _smallprimes = sp
 
-    factor = gmpy2.gcd(smallprimes, n)
+    factor = gmpy2.gcd(_smallprimes, n)
     if factor != 1:
         return {"p": factor, "q": n // factor}
     return False
