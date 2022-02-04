d00578ee12530e61a93e61e10df2137f3e61791e
Hanno Böck <hanno@gentoo.org>
2022-02-04 22:09:47 +0100

    test for pattern key check

---
+++ tests/test_pattern.py
@@ -0,0 +1,23 @@
+import unittest
+import os
+
+import badkeys
+
+TDPATH = f"{os.path.dirname(__file__)}/data/"
+
+
+class TestRoca(unittest.TestCase):
+
+    def test_roca(self):
+        with open(f"{TDPATH}rsa-pattern.key") as f:
+            key = f.read()
+        r = badkeys.checkpkey(key, checks=["pattern"])
+        self.assertTrue("pattern" in r["results"])
+        with open(f"{TDPATH}rsa-ok.key") as f:
+            key = f.read()
+        r = badkeys.checkpkey(key, checks=["pattern"])
+        self.assertFalse(r["results"])
+
+
+if __name__ == '__main__':
+    unittest.main()
