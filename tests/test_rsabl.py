4126c4d221bc5a8b104b1d66306fef68771692bf
Hanno Böck <hanno@gentoo.org>
2022-02-13 17:53:00 +0100

    add blocklist test with debian weak key

---
+++ tests/test_rsabl.py
@@ -0,0 +1,23 @@
+import unittest
+import os
+
+import badkeys
+
+TDPATH = f"{os.path.dirname(__file__)}/data/"
+
+
+class TestRsabl(unittest.TestCase):
+
+    def test_rsabl(self):
+        with open(f"{TDPATH}rsa-debianweak.key") as f:
+            key = f.read()
+        r = badkeys.checkpkey(key, checks=["rsabl"])
+        self.assertTrue("rsabl" in r["results"])
+        with open(f"{TDPATH}rsa-ok.key") as f:
+            key = f.read()
+        r = badkeys.checkpkey(key, checks=["rsabl"])
+        self.assertFalse(r["results"])
+
+
+if __name__ == '__main__':
+    unittest.main()
