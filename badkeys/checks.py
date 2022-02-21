21e113573efd598ad2fc92dd5f36190af8c6d349
Hanno Böck <hanno@gentoo.org>
2022-02-21 13:46:59 +0100

    support private keys (pkcs #1/#8)

--- badkeys/checks.py
+++ badkeys/checks.py
@@ -1,6 +1,6 @@
 from cryptography import x509
 from cryptography.hazmat.primitives.asymmetric import rsa
-from cryptography.hazmat.primitives.serialization import load_pem_public_key
+from cryptography.hazmat.primitives import serialization
 
 from .rsakeys import fermat
 from .rsakeys import pattern
@@ -44,20 +44,7 @@ allchecks = {
 }
 
 
-def checkrsa(n, e=65537, checks=allchecks.keys()):
-    results = {}
-    for check in checks:
-        if allchecks[check]["type"] != "rsa":
-            continue
-        callcheck = allchecks[check]["function"]
-        r = callcheck(n, e=e)
-        if r is not False:
-            results[check] = r
-    return results
-
-
-def checkpkey(rawkey, checks=allchecks.keys()):
-    key = load_pem_public_key(rawkey.encode())
+def _checkkey(key, checks):
     r = {}
     if isinstance(key, rsa.RSAPublicKey):
         r["type"] = "rsa"
@@ -71,32 +58,36 @@ def checkpkey(rawkey, checks=allchecks.keys()):
     return r
 
 
+def checkrsa(n, e=65537, checks=allchecks.keys()):
+    results = {}
+    for check in checks:
+        if allchecks[check]["type"] != "rsa":
+            continue
+        callcheck = allchecks[check]["function"]
+        r = callcheck(n, e=e)
+        if r is not False:
+            results[check] = r
+    return results
+
+
+def checkpkey(rawkey, checks=allchecks.keys()):
+    key = serialization.load_pem_public_key(rawkey.encode())
+    return _checkkey(key, checks)
+
+
+def checkprivkey(rawkey, checks=allchecks.keys()):
+    priv = serialization.load_pem_private_key(rawkey.encode(), password=None)
+    return _checkkey(priv.public_key(), checks)
+
+
 def checkcrt(rawcert, checks=allchecks.keys()):
     crt = x509.load_pem_x509_certificate(rawcert.encode())
-    r = {}
-    if isinstance(crt.public_key(), rsa.RSAPublicKey):
-        r["type"] = "rsa"
-        r["n"] = crt.public_key().public_numbers().n
-        r["e"] = crt.public_key().public_numbers().e
-        r["bits"] = r["n"].bit_length()
-        r["results"] = checkrsa(r["n"], e=r["e"], checks=checks)
-    else:
-        r["type"] = "unsupported"
-    return r
+    return _checkkey(crt.public_key(), checks)
 
 
 def checkcsr(rawcsr, checks=allchecks.keys()):
     csr = x509.load_pem_x509_csr(rawcsr.encode())
-    r = {}
-    if isinstance(csr.public_key(), rsa.RSAPublicKey):
-        r["type"] = "rsa"
-        r["n"] = csr.public_key().public_numbers().n
-        r["e"] = csr.public_key().public_numbers().e
-        r["bits"] = r["n"].bit_length()
-        r["results"] = checkrsa(r["n"], e=r["e"], checks=checks)
-    else:
-        r["type"] = "unsupported"
-    return r
+    return _checkkey(csr.public_key(), checks)
 
 
 def detectandcheck(inkey, checks=allchecks.keys()):
@@ -108,3 +99,7 @@ def detectandcheck(inkey, checks=allchecks.keys()):
         return checkpkey(inkey, checks)
     elif "-----BEGIN RSA PUBLIC KEY-----" in inkey:
         return checkpkey(inkey, checks)
+    elif "-----BEGIN PRIVATE KEY-----" in inkey:
+        return checkprivkey(inkey, checks)
+    elif "-----BEGIN RSA PRIVATE KEY-----" in inkey:
+        return checkprivkey(inkey, checks)
