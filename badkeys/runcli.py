7bfca85cc90a87f15a5f6f8b524a18d80e015732
Hanno Böck <hanno@gentoo.org>
2022-03-11 17:09:31 +0100

    add TLS scan mode

--- badkeys/runcli.py
+++ badkeys/runcli.py
@@ -1,8 +1,9 @@
 import sys
 import argparse
 import signal
+import ssl
 
-from .checks import detectandcheck, allchecks, checkrsa
+from .checks import detectandcheck, allchecks, checkrsa, checkcrt
 
 MAXINPUTSIZE = 10000
 
@@ -26,6 +27,10 @@ def runcli():
                     help="Input file is list of RSA hex moduli")
     ap.add_argument("-d", "--debug", action="store_true",
                     help="Output debug messages")
+    ap.add_argument("-t", "--tls", action="store_true",
+                    help="Scan TLS (pass hostnames or IPs instead of files)")
+    ap.add_argument("-p", "--ports",
+                    help="Ports to scan (TLS mode)")
     args = ap.parse_args()
 
     if args.checks:
@@ -35,6 +40,34 @@ def runcli():
                 sys.exit(f"{c} is not a valid check")
     else:
         userchecks = allchecks.keys()
+
+    if args.tls:
+        if args.ports:
+            ports = []
+            for p in args.ports.split(','):
+                ports += int(p)
+        else:
+            # ports for https, smtps, imaps, pop3s, ldaps, ftps
+            # and 8443 as most common non-default https port
+            ports = [443, 465, 993, 995, 465, 636, 990, 8443]
+        for host in args.infiles:
+            for port in ports:
+                try:
+                    cert = ssl.get_server_certificate((host, port))
+                except ConnectionRefusedError:
+                    continue
+                r = checkcrt(cert, checks=userchecks)
+                if r['type'] == "unsupported":
+                    print("Warning: Unsupported key type", file=sys.stderr)
+                for check, result in r['results'].items():
+                    sub = ""
+                    if 'subtest' in result:
+                        sub = f"/{result['subtest']}"
+                    print(f"{check}{sub} vulnerability found {host}:{port}")
+                    if args.debug and "debug" in result:
+                        print(result["debug"])
+
+        sys.exit(1)
 
     for fn in args.infiles:
         if fn == "-":
