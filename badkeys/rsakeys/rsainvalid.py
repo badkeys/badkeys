1cbdc2d35f23a99446a5af13ddfd357a9282d32c
Hanno Böck <hanno@gentoo.org>
2022-05-16 19:05:12 +0200

    e larger or equal N is invalid

--- badkeys/rsakeys/rsainvalid.py
+++ badkeys/rsakeys/rsainvalid.py
@@ -4,6 +4,8 @@ import gmpy2
 def rsainvalid(n, e=65537):
     if e < 3:
         return {"detected": True, "subtest": "invalid_e"}
+    if e >= n:
+        return {"detected": True, "subtest": "e_too_large"}
     if gmpy2.is_prime(n):
         return {"detected": True, "subtest": "prime_n"}
     return False
