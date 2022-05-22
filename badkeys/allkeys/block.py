d172f6ffde228cfa53407737b6e2684c8369c40a
Hanno Böck <hanno@gentoo.org>
2022-05-22 09:19:26 +0200

    use blocklist.dat from cache dir

--- badkeys/allkeys/block.py
+++ badkeys/allkeys/block.py
@@ -1,35 +1,33 @@
 import hashlib
 import mmap
-from importlib.resources import open_binary
+import pathlib
+import json
 
+_blmeta = False
 _bldata = False
 
 
 def blocklist(inval):
-    global _bldata
+    global _blmeta, _bldata
+
+    mlist = {}
+    if not _blmeta:
+        with open(f"{cachedir}badkeysdata.json") as f:
+            jdata = json.loads(f.read())
+        for bl in jdata["blocklists"]:
+            blid = int(bl["id"])
+            mlist[blid] = {"name": bl["name"]}
+        _blmeta = mlist
 
     if not _bldata:
-        with open_binary("badkeys.keydata", "blocklist.dat") as f:
+        cachedir = str(pathlib.Path.home()) + "/.cache/badkeys/"
+        print(cachedir)
+        with open(f"{cachedir}blocklist.dat", "rb") as f:
             _bldata = mmap.mmap(f.fileno(), 0, prot=mmap.PROT_READ)
 
     inval_b = inval.to_bytes((inval.bit_length() + 7) // 8, byteorder="big")
 
     s256trunc = hashlib.sha256(inval_b).digest()[:15]
-
-    mlists = {
-        1: "debianssl",
-        2: "debianssh-x86",
-        3: "debianssh-x64",
-        5: "keypair",
-        21: "documentation",
-        22: "firmware",
-        23: "localhostcert",
-        24: "localroot",
-        25: "misc",
-        26: "rfc",
-        27: "softwaretests",
-        28: "testvectors",
-    }
 
     fbegin = 0
     fend = (len(_bldata) // 16) - 1
@@ -38,8 +36,8 @@ def blocklist(inval):
         val = _bldata[fmiddle * 16 : fmiddle * 16 + 15]
         if s256trunc == val:
             bl_id = int(_bldata[fmiddle * 16 + 15])
-            if bl_id in mlists:
-                subtest = mlists[bl_id]
+            if bl_id in _blmeta:
+                subtest = _blmeta[bl_id]["name"]
             else:
                 subtest = f"id{bl_id}"
             return {
