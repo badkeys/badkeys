c11b5a03b2f61b692bf58385e12acd44c041e752
Hanno Böck <990588+hannob@users.noreply.github.com>
2024-04-22 19:01:46 +0200

    Remove check if RSA N is prime
    The rsainvalid module contains basic checks for defect RSA keys.
    The RSA N value is supposed to be a composite number, we were
    checking if it is a prime.
    This check has not much value (such keys would simply not work), but
    it is computationally expensive. badkeys is ~10x faster without it.
    
    Closes: https://github.com/badkeys/badkeys/issues/16

--- badkeys/rsakeys/rsainvalid.py
+++ badkeys/rsakeys/rsainvalid.py
@@ -6,6 +6,4 @@ def rsainvalid(n, e=65537):
         return {"detected": True, "subtest": "invalid_e"}
     if e >= n:
         return {"detected": True, "subtest": "e_too_large"}
-    if gmpy2.is_prime(n):
-        return {"detected": True, "subtest": "prime_n"}
     return False
