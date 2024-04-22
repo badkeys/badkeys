236a7009c3d693673368eee636d9b475a56d26e5
Hanno Böck <990588+hannob@users.noreply.github.com>
2024-04-22 19:14:07 +0200

    Remove leftovers from rsainvalid n_prime test (unused import, tests)
    Also fix rsainvalid test function names.

--- badkeys/rsakeys/rsainvalid.py
+++ badkeys/rsakeys/rsainvalid.py
@@ -1,6 +1,3 @@
-import gmpy2
-
-
 def rsainvalid(n, e=65537):
     if e < 3:
         return {"detected": True, "subtest": "invalid_e"}
--- tests/test_rsainvalid.py
+++ tests/test_rsainvalid.py
@@ -6,19 +6,13 @@ import badkeys
 TDPATH = f"{os.path.dirname(__file__)}/data/"
 
 
-class TestSmallprimes(unittest.TestCase):
-    def test_smallprimes(self):
+class TestRSAInvalid(unittest.TestCase):
+    def test_rsainvalid(self):
         with open(f"{TDPATH}rsa-e1.key") as f:
             key = f.read()
         r = badkeys.checkpubkey(key, checks=["rsainvalid"])
         self.assertTrue("rsainvalid" in r["results"])
         self.assertEqual("invalid_e", r["results"]["rsainvalid"]["subtest"])
-
-        with open(f"{TDPATH}rsa-nprime.key") as f:
-            key = f.read()
-        r = badkeys.checkpubkey(key, checks=["rsainvalid"])
-        self.assertTrue("rsainvalid" in r["results"])
-        self.assertEqual("prime_n", r["results"]["rsainvalid"]["subtest"])
 
         with open(f"{TDPATH}rsa-ok.key") as f:
             key = f.read()
