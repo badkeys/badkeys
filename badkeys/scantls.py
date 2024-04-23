8cce2b3ceec9d677cc298557a04b5642fe818fab
Hanno Böck <990588+hannob@users.noreply.github.com>
2024-04-23 21:23:40 +0200

    remove unused variable

--- badkeys/scantls.py
+++ badkeys/scantls.py
@@ -8,6 +8,6 @@ def scantls(host, port=443, checks=allchecks.keys()):
         # this currently does not support multiple
         # certificates on one host
         cert = ssl.get_server_certificate((host, port))
-    except (ConnectionRefusedError, ssl.SSLError, OSError) as e:
+    except (ConnectionRefusedError, ssl.SSLError, OSError):
         return []
     return [checkcrt(cert, checks=checks)]
