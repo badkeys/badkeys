17be27ad023e5431edfa1bed1a839ee25ed61a9b
Hanno Böck <990588+hannob@users.noreply.github.com>
2024-04-29 15:24:17 +0200

    Move blocklist tests to one file

--- tests/test_blocklist.py
+++ tests/test_blocklist.py
@@ -6,7 +6,19 @@ import badkeys
 TDPATH = f"{os.path.dirname(__file__)}/data/"
 
 
-class TestEcbl(unittest.TestCase):
+class TestBlocklist(unittest.TestCase):
+
+    @unittest.skipUnless(os.environ.get("RUNBLTESTS"), "Not running blocklist tests")
+    def test_rsabl(self):
+        with open(f"{TDPATH}rsa-debianweak.key") as f:
+            key = f.read()
+        r = badkeys.checkpubkey(key, checks=["blocklist"])
+        self.assertTrue("blocklist" in r["results"])
+        with open(f"{TDPATH}rsa-ok.key") as f:
+            key = f.read()
+        r = badkeys.checkpubkey(key, checks=["blocklist"])
+        self.assertFalse(r["results"])
+
     @unittest.skipUnless(os.environ.get("RUNBLTESTS"), "Not running blocklist tests")
     def test_ecbl(self):
         with open(f"{TDPATH}ec-p256-rfc-example.key") as f:
