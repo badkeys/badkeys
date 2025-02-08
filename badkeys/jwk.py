d46b6d5e500ae5e788c9e1f12f42761fb32378ce
Hanno Böck <990588+hannob@users.noreply.github.com>
2025-02-08 22:22:10 +0100

    Catch some cases of invalid JSON Web Keys

--- badkeys/jwk.py
+++ badkeys/jwk.py
@@ -13,16 +13,28 @@ def _ub64toint(b64):
 def checkjwk(key, checks):
     r = {}
     if key["kty"] == "RSA":
+        if "n" not in key or "e" not in key or key["n"] == "" or key["e"] == "":
+            r["type"] = "unparseable"
+            r["results"] = {}
+            return r
         r["type"] = "rsa"
         r["n"] = _ub64toint(key["n"])
         r["e"] = _ub64toint(key["e"])
         r["results"] = checkrsa(r["n"], e=r["e"], checks=checks)
     elif key["kty"] == "EC":
+        if "x" not in key or "y" not in key or key["x"] == "" or key["y"] == "":
+            r["type"] = "unparseable"
+            r["results"] = {}
+            return r
         r["type"] = "ec"
         r["x"] = _ub64toint(key["x"])
         r["y"] = _ub64toint(key["x"])
         r["results"] = checkall(r["x"], checks=checks)
     elif key["kty"] == "OKP":
+        if "x" not in key or key["x"] == "":
+            r["type"] = "unparseable"
+            r["results"] = {}
+            return r
         r["type"] = "ec"
         r["x"] = _ub64toint(key["x"])
         # no y coordinate for ed25519/ed448
