ee6c510cfb97dbaf4183e6626e7564b9b69c81bb
Hanno Böck <990588+hannob@users.noreply.github.com>
2025-04-10 10:24:30 +0200

    Regression test for RSA N=0 key
    Bug: https://github.com/badkeys/badkeys/issues/31

---
+++ tests/test_regression.py
@@ -0,0 +1,21 @@
+# Tests for bugs in past versions of badkeys
+
+import os
+import unittest
+import pathlib
+
+import badkeys
+
+TDPATH = f"{os.path.dirname(__file__)}/data/"
+
+
+class TestRegressions(unittest.TestCase):
+    def test_rsanzero(self):
+        # Regression test for https://github.com/badkeys/badkeys/issues/31
+        key = pathlib.Path(f"{TDPATH}rsazerozero.key").read_text()
+        r = badkeys.checkpubkey(key, checks=["xzbackdoor"])
+        self.assertFalse(r["results"])
+
+
+if __name__ == "__main__":
+    unittest.main()
