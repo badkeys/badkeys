48a6ea71461c95640545006109bc62a0b7d7b313
Hanno Böck <990588+hannob@users.noreply.github.com>
2025-04-19 21:25:16 +0200

    Use cached function for cachedir instead of generating path each time
    Adds support for XDG_CACHE_HOME variable.
    This also reworks some related code to use simpler functions
    and avoid unneeded conversions of binary data to strings.

--- badkeys/update.py
+++ badkeys/update.py
@@ -6,10 +6,10 @@ import pathlib
 import sys
 import urllib.request
 
-from .utils import _warnmsg
+from .utils import _cachedir, _warnmsg
 
 
-def _dlxz(url, filename, exphash, cachedir, quiet):
+def _dlxz(url, filename, exphash, quiet):
     if not quiet:
         print(f"Downloading {filename}...")
     dldata = urllib.request.urlopen(url).read()
@@ -17,24 +17,24 @@ def _dlxz(url, filename, exphash, cachedir, quiet):
     dlhash = hashlib.sha256(dlunpacked).hexdigest()
     if dlhash != exphash:
         sys.exit(f"ERROR: SHA256 hash of downloaded {filename} does not match")
-    pathlib.Path(f"{cachedir}_{filename}.tmp").write_bytes(dlunpacked)
-    os.replace(f"{cachedir}_{filename}.tmp", f"{cachedir}{filename}")
+    tmpfile = os.path.join(_cachedir(), f"_{filename}.tmp")
+    pathlib.Path(tmpfile).write_bytes(dlunpacked)
+    os.replace(tmpfile, os.path.join(_cachedir(), filename))
 
 
 def update_bl(lookup=False, quiet=False):
     UPDATEURL = "https://update.badkeys.info/"
     BKFORMAT = 0
 
-    cachedir = str(pathlib.Path.home()) + "/.cache/badkeys/"
-    if not os.path.exists(cachedir):
-        pathlib.Path(cachedir).mkdir(parents=True)
+    if not os.path.exists(_cachedir()):
+        pathlib.Path(_cachedir()).mkdir(parents=True)
 
     jurl = f"{UPDATEURL}/v{BKFORMAT}/badkeysdata.json"
-    bkdata = urllib.request.urlopen(jurl).read().decode()
+    bkdata = urllib.request.urlopen(jurl).read()
     bkdata_old = ""
-    if os.path.exists(f"{cachedir}badkeysdata.json"):
-        with open(f"{cachedir}badkeysdata.json") as f:
-            bkdata_old = f.read()
+    bkjsonfile = os.path.join(_cachedir(), "badkeysdata.json")
+    if os.path.exists(bkjsonfile):
+        bkdata_old = pathlib.Path(bkjsonfile).read_bytes()
 
     data = json.loads(bkdata)
     if data["bkformat"] != BKFORMAT:
@@ -46,26 +46,25 @@ def update_bl(lookup=False, quiet=False):
     else:
         if not quiet:
             print("Writing new badkeysdata.json...")
-        with open(f"{cachedir}badkeysdata.json", "w") as f:
-            f.write(bkdata)
+        pathlib.Path(bkjsonfile).write_bytes(bkdata)
 
     # starting with blocklist.dat
     oldbl_sha256 = ""
-    if os.path.exists(f"{cachedir}blocklist.dat"):
-        with open(f"{cachedir}blocklist.dat", "rb") as f:
+    if os.path.exists(os.path.join(_cachedir(), "blocklist.dat")):
+        with open(os.path.join(_cachedir(), "blocklist.dat"), "rb") as f:
             oldbl_sha256 = hashlib.sha256(f.read()).hexdigest()
 
     if oldbl_sha256 != data["blocklist_sha256"]:
-        _dlxz(data["blocklist_url"], "blocklist.dat", data["blocklist_sha256"], cachedir, quiet)
+        _dlxz(data["blocklist_url"], "blocklist.dat", data["blocklist_sha256"], quiet)
 
     # starting with lookup.txt
     oldlu_sha256 = ""
-    if os.path.exists(f"{cachedir}lookup.txt"):
-        with open(f"{cachedir}lookup.txt", "rb") as f:
+    if os.path.exists(os.path.join(_cachedir(), "lookup.txt")):
+        with open(os.path.join(_cachedir(), "lookup.txt"), "rb") as f:
             oldlu_sha256 = hashlib.sha256(f.read()).hexdigest()
         if not lookup and (oldlu_sha256 != data["lookup_sha256"]):
             _warnmsg("Old lookup.txt file found.")
             _warnmsg("You may want to run --update-bl-and-urls")
 
     if lookup and (oldlu_sha256 != data["lookup_sha256"]):
-        _dlxz(data["lookup_url"], "lookup.txt", data["lookup_sha256"], cachedir, quiet)
+        _dlxz(data["lookup_url"], "lookup.txt", data["lookup_sha256"], quiet)
--- badkeys/utils.py
+++ badkeys/utils.py
@@ -1,5 +1,15 @@
+import functools
+import os
 import sys
 
 
 def _warnmsg(warnmsg):
     print(f"WARNING: {warnmsg}", file=sys.stderr)
+
+
+@functools.cache
+def _cachedir():
+    cachedir = os.getenv("XDG_CACHE_HOME")
+    if cachedir:
+        return cachedir
+    return os.path.expanduser("~/.cache/badkeys/")
