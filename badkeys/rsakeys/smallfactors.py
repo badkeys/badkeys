fb52ab538fe0d1c01d0c43bda005cf38739445d8
Hanno Böck <990588+hannob@users.noreply.github.com>
2025-04-19 21:00:33 +0200

    Use functools cache instead of global var to avoid recalculating smallprimes constant

--- badkeys/rsakeys/smallfactors.py
+++ badkeys/rsakeys/smallfactors.py
@@ -1,28 +1,28 @@
+import functools
+
 import gmpy2
 
 MAX_PRIME = 65537
 
-_smallprimes = None
+
+@functools.cache
+def _gensmallprimes():
+    # Generate product of all primes <= MAX_PRIME.
+    # We calculate this once per program run, we could precalculate the
+    # constant, but it's fast enough to calculate on the fly.
+    sp = prime = 2
+    while prime < MAX_PRIME:
+        prime = gmpy2.next_prime(prime)
+        sp *= prime
+    return sp
 
 
 def smallfactors(n, e=0):  # noqa: ARG001
-    global _smallprimes
-
     # Don't try to factor nonsensical keys
     if n <= 5:
         return False
 
-    # Generate product of all primes <= MAX_PRIME.
-    # We calculate this once per program run, we could precalculate the
-    # constant, but it's fast enough to calculate on the fly.
-    if _smallprimes is None:
-        sp = prime = 2
-        while prime < MAX_PRIME:
-            prime = gmpy2.next_prime(prime)
-            sp *= prime
-        _smallprimes = sp
-
-    factor = gmpy2.gcd(_smallprimes, n)
+    factor = gmpy2.gcd(_gensmallprimes(), n)
     if factor != 1:
         return {"p": int(factor), "q": int(n // factor)}
     return False
