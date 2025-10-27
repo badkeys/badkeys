22d71f51a03e67042b913793e4bd616ac81cfce6
Hanno Böck <990588+hannob@users.noreply.github.com>
2025-10-27 09:42:10 +0100

    Add test for various unsupported and defect inputs

--- tests/test_failures.py
+++ tests/test_failures.py
@@ -1,4 +1,5 @@
 import os
+import pathlib
 import unittest
 
 import badkeys
@@ -24,6 +25,22 @@ class TestFailures(unittest.TestCase):
     def test_unsupportedssh(self):
         r = badkeys.detectandcheck("ssh-invalid invalid")
         self.assertEqual(r["type"], "unsupported")
+
+    def test_defect(self):
+        indir = os.path.join(TDPATH, "defect")
+        for fn in os.listdir(indir):
+            indata = pathlib.Path(os.path.join(indir, fn)).read_text()
+            r = badkeys.detectandcheck(indata)
+            self.assertEqual(r["type"], "unparseable")
+
+    def test_unsupported(self):
+        indir = os.path.join(TDPATH, "unsupported")
+        for fn in os.listdir(indir):
+            indata = pathlib.Path(os.path.join(indir, fn)).read_text()
+            r = badkeys.detectandcheck(indata)
+            # some "unsupported" inputs return "unparseable" due to
+            # cryptography's API behavior
+            self.assertIn(r["type"], ["unparseable", "unsupported"])
 
 
 if __name__ == "__main__":
