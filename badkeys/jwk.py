8b12e05c45fe02e59b406fdd2e53620aeade026c
Hanno Böck <990588+hannob@users.noreply.github.com>
2026-01-02 10:37:26 +0100

    Catch various cases of invalid JWK structures
    Various JWK inputs with unexpected types caused unexpected exceptions,
    e.g.:
    ```
    123
    {"kty":"RSA", "n":{}, "e":{}}
    {"kty":"EC", "x":{}, "y":{}, "crv": "P-256"}
    ```

--- badkeys/jwk.py
+++ badkeys/jwk.py
@@ -16,16 +16,13 @@ def _ub64toint(b64):
 
 def checkjwk(key, checks):
     r = {}
+    if not isinstance(key, dict) or "kty" not in key:
+        return {"type": "unparseable", "results": {}}
     try:
-        if "kty" not in key:
-            r["type"] = "unparseable"
-            r["results"] = {}
-            return r
         if key["kty"] == "RSA":
-            if "n" not in key or "e" not in key or key["n"] == "" or key["e"] == "":
-                r["type"] = "unparseable"
-                r["results"] = {}
-                return r
+            if "n" not in key or "e" not in key or key["n"] == "" or key["e"] == "" \
+               or not isinstance(key["n"], str) or not isinstance(key["e"], str):
+                return {"type": "unparseable", "results": {}}
             r["type"] = "rsa"
             r["n"] = _ub64toint(key["n"])
             r["e"] = _ub64toint(key["e"])
@@ -33,10 +30,9 @@ def checkjwk(key, checks):
             r["results"] = checkrsa(r["n"], e=r["e"], checks=checks)
         elif key["kty"] == "EC":
             if "x" not in key or "y" not in key or key["x"] == "" or key["y"] == "" \
+               or not isinstance(key["x"], str) or not isinstance(key["y"], str) \
                or "crv" not in key or key["crv"] not in VALIDCURVES:
-                r["type"] = "unparseable"
-                r["results"] = {}
-                return r
+                return {"type": "unparseable", "results": {}}
             r["type"] = "ec"
             r["curve"] = key["crv"].lower().replace("-", "")
             r["x"] = _ub64toint(key["x"])
@@ -44,19 +40,16 @@ def checkjwk(key, checks):
             r["results"] = checkall(r["x"], checks=checks)
         elif key["kty"] == "OKP":
             if "x" not in key or key["x"] == "" \
+               or not isinstance(key["x"], str) \
                or "crv" not in key or key["crv"] not in VALIDCURVES:
-                r["type"] = "unparseable"
-                r["results"] = {}
-                return r
+                return {"type": "unparseable", "results": {}}
             r["type"] = "ec"
             r["curve"] = key["crv"].lower()
             r["x"] = _ub64toint(key["x"])
             # no y coordinate for ed25519/ed448
             r["results"] = checkall(r["x"], checks=checks)
         else:
-            r["type"] = "unsupported"
-            r["results"] = {}
+            return {"type": "unsupported", "results": {}}
     except (binascii.Error, UnicodeEncodeError):
-        r["type"] = "unparseable"
-        r["results"] = {}
+        return {"type": "unparseable", "results": {}}
     return r
