ca202d55efd6bbe4c6aaf24b7321c183698259ee
Hanno Böck <990588+hannob@users.noreply.github.com>
2026-04-28 15:29:47 +0200

    Test explicit curve parameter key
    Previously, Python Cryptography didn't support explicit curve parameters
    and we had a test that it would return "unsupported". The latest version
    (47.0.0) added support, so test that.
    While touching the blocklist test file, convert it to use pathlib.

--- tests/test_blocklist.py
+++ tests/test_blocklist.py
@@ -1,4 +1,5 @@
 import os
+import pathlib
 import unittest
 
 import badkeys
@@ -10,42 +11,42 @@ class TestBlocklist(unittest.TestCase):
 
     @unittest.skipUnless(os.environ.get("RUN_ONLINETESTS"), "Skipping blocklist tests")
     def test_rsabl(self):
-        with open(f"{TDPATH}rsa-debianweak.key") as f:
-            key = f.read()
+        key = pathlib.Path(f"{TDPATH}rsa-debianweak.key").read_text()
         r = badkeys.checkpubkey(key, checks=["blocklist"])
         self.assertTrue("blocklist" in r["results"])
         self.assertTrue(r["bits"] == 2048)
-        with open(f"{TDPATH}rsa-ok.key") as f:
-            key = f.read()
+        key = pathlib.Path(f"{TDPATH}rsa-ok.key").read_text()
         r = badkeys.checkpubkey(key, checks=["blocklist"])
         self.assertFalse(r["results"])
         self.assertTrue(r["bits"] == 2048)
 
     @unittest.skipUnless(os.environ.get("RUN_ONLINETESTS"), "Skipping blocklist tests")
     def test_ecbl(self):
-        with open(f"{TDPATH}ec-p256-rfc-example.key") as f:
-            key = f.read()
+        key = pathlib.Path(f"{TDPATH}ec-p256-rfc-example.key").read_text()
         r = badkeys.checkpubkey(key, checks=["blocklist"])
         self.assertTrue("blocklist" in r["results"])
         self.assertTrue(r["curve"] == "p256")
-        with open(f"{TDPATH}ed25519-rfc-example.key") as f:
-            key = f.read()
+        key = pathlib.Path(f"{TDPATH}ed25519-rfc-example.key").read_text()
         r = badkeys.checkpubkey(key, checks=["blocklist"])
         self.assertTrue("blocklist" in r["results"])
         self.assertTrue(r["curve"] == "ed25519")
-        with open(f"{TDPATH}x448-ok.key") as f:
-            key = f.read()
+        key = pathlib.Path(f"{TDPATH}x448-ok.key").read_text()
         r = badkeys.checkpubkey(key, checks=["blocklist"])
         self.assertFalse(r["results"])
         self.assertTrue(r["curve"] == "x448")
+        # RFC example ECDSA key with explicit curve encoding
+        key = pathlib.Path(f"{TDPATH}ecdsa-explicit-pub.key").read_text()
+        r = badkeys.checkpubkey(key, checks=["blocklist"])
+        self.assertTrue("blocklist" in r["results"])
+        self.assertTrue(r["results"]["blocklist"]["subtest"] == "rfc")
+        self.assertTrue(r["curve"] == "p256")
 
     # Testing key in SSH DSA pubkey format.
     # Python cryptography plans to deprecate this format,
     # we will need to find a solution.
     @unittest.skipUnless(os.environ.get("RUN_ONLINETESTS"), "Skipping blocklist tests")
     def test_dsabl(self):
-        with open(f"{TDPATH}dsa-sshpub-ietf-example.key") as f:
-            key = f.read()
+        key = pathlib.Path(f"{TDPATH}dsa-sshpub-ietf-example.key").read_text()
         r = badkeys.checksshpubkey(key, checks=["blocklist"])
         self.assertTrue("blocklist" in r["results"])
         self.assertTrue(r["bits"] == 1024)
