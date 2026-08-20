9372dd3e04d7de56fd07f44c06958c82c1b43288
Hanno Böck <990588+hannob@users.noreply.github.com>
2026-08-20 22:05:20 +0200

    Raise version to 0.0.20

--- badkeys/__init__.py
+++ badkeys/__init__.py
@@ -1,4 +1,4 @@
-__version__ = "0.0.19"
+__version__ = "0.0.20"
 __all__ = ["allchecks", "checkcrt", "checkcsr", "checkdnskey", "checkjwk", "checkprivkey",
            "checkpubkey", "checkrsa", "checksshpubkey", "defaultchecks", "detectandcheck",
            "scanssh", "warningchecks"]
